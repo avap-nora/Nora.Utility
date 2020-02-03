f6b2654976abb7885ae3cd897cc44e5c54ebede2
Hajime MATSUMOTO <hajime.matsumoto@avap.co.jp>
2020-02-03 10:17:37 +0900

    fix namespace

--- src/FileSystem/CreateWritableDirectory.php
+++ src/FileSystem/CreateWritableDirectory.php
@@ -4,15 +4,19 @@
  */
 declare(strict_types=1);
 
-namespace Nora\System\FileSystem;
+namespace Nora\Utility\FileSystem;
 
 class CreateWritableDirectory
 {
     public function __invoke(...$parts)
     {
-        $directory = "/".implode("/", array_map(function ($value) {
-            return trim($value, '/');
-        }, $parts));
+        $directory = "/".implode(
+            "/", array_map(
+                function ($value) {
+                    return trim($value, '/');
+                }, $parts
+            )
+        );
 
         if (!file_exists($directory) && !@mkdir($directory, 0777, true) && !is_dir($directory)) {
             throw new NotWritableException($directory);
